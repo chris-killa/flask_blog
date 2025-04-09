aba4120fd43512968e3f0c6cebb55ed14c22ea40
hecking_killa <chris.hermann9397@gmail.com>
2025-04-09 17:52:25 +0200

    more functionalities and bug fixing

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
-from data.storage import load_posts
+from flask import Flask, render_template, request, redirect, url_for
+from data.storage import load_posts, save_posts
+
 app = Flask(__name__)
 
 
@@ -8,6 +9,29 @@ def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
 
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        blog_posts = load_posts()
+        new_post = {
+            "id" : len(blog_posts)+1,
+            "title" : request.form.get('title'),
+            "content" : request.form.get('content'),
+            "author" : request.form.get('author')
+        }
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    blog_posts = load_posts()
+    blog_posts = [post for post in blog_posts if post['id'] != post_id]
+    save_posts(blog_posts)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5004, debug=True)
--- data/storage.py
+++ data/storage.py
@@ -3,17 +3,25 @@ import json
 
 DATA_FILE = os.path.join('data', 'posts.json')
 
+
 def load_posts():
+
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
     if not os.path.exists(DATA_FILE):
         example_posts = [
             {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
             {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'}
         ]
-        save_posts(example_posts)  # direkt speichern
+        save_posts(example_posts)
         return example_posts
+
+
     with open(DATA_FILE, 'r') as f:
         return json.load(f)
 
 def save_posts(posts):
     with open(DATA_FILE, 'w') as f:
         json.dump(posts, f, indent=4)
+
